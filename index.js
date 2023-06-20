4bd168d30ac5d0a036b8a92b4a793c119b642f1f
N. J <datavispresent@gmail.com>
2023-06-20 21:29:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -120,6 +120,7 @@ app.get('/', async (req, res) => {
     res.send('Data processed.');
 });
 
-app.listen(3000, () => {
-    console.log('Server is up and running on port 3000.');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is up and running on port ${process.env.PORT || 3000}.`);
 });
+
